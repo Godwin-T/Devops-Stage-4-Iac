3a98a4737a739cb99f93ecad5745d97fdc80c73e
Godwin-T <godwinolagunju0@gmail.com>
2025-02-28 20:23:46 +0100

    update

--- terraform/inventory.tpl
+++
@@ -1,3 +0,0 @@
-[web]
-${aws_instance.web.public_ip} ansibleuser=ubuntu ansible_ssh_private_key_file=~/.ssh/playerball.pem
-_
--- terraform/main.tf
+++ terraform/main.tf
@@ -65,7 +65,7 @@ resource "null_resource" "ansible_provision" {
   }
 
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory ../ansible/playbook.yml"
+    command = "sleep 60 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory ../ansible/playbook.yml"
   }
 }
 
